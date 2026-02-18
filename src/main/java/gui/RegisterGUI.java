2262558badc85dce87eb1e5e4538beccf6934558
Sami <sashrafbibi@gmail.com>
2026-02-18 12:55:19 +0100

    Fuck off

--- src/main/java/gui/RegisterGUI.java
+++ src/main/java/gui/RegisterGUI.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.awt.EventQueue;
+import java.util.ResourceBundle;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -46,32 +47,35 @@ public class RegisterGUI extends JFrame {
 		contentPane.setLayout(null);
 
 		JLabel lblRegister = new JLabel("New label");
-		lblRegister.setBounds(67, 32, 60, 17);
+		lblRegister.setBounds(33, 40, 120, 17);
 		contentPane.add(lblRegister);
+		lblRegister.setText(ResourceBundle.getBundle("Etiquetas").getString("RegisterGUI.NewUser"));
 
 		JLabel lblPass1 = new JLabel("New label");
-		lblPass1.setBounds(67, 83, 60, 17);
+		lblPass1.setBounds(33, 80, 120, 17);
 		contentPane.add(lblPass1);
+		lblPass1.setText(ResourceBundle.getBundle("Etiquetas").getString("RegisterGUI.Password1"));
 
 		JLabel lblPass2 = new JLabel("New label");
-		lblPass2.setBounds(67, 137, 60, 17);
+		lblPass2.setBounds(33, 120, 120, 17);
 		contentPane.add(lblPass2);
+		lblPass2.setText(ResourceBundle.getBundle("Etiquetas").getString("RegisterGUI.Password2"));
 
 		textFieldRegister = new JTextField();
-		textFieldRegister.setBounds(173, 30, 114, 21);
+		textFieldRegister.setBounds(175, 40, 114, 21);
 		contentPane.add(textFieldRegister);
 		textFieldRegister.setColumns(10);
 
 		passwordFieldPass1 = new JPasswordField();
-		passwordFieldPass1.setBounds(173, 81, 114, 21);
+		passwordFieldPass1.setBounds(175, 80, 114, 21);
 		contentPane.add(passwordFieldPass1);
 
 		passwordFieldPass2 = new JPasswordField();
-		passwordFieldPass2.setBounds(173, 135, 114, 21);
+		passwordFieldPass2.setBounds(175, 120, 114, 21);
 		contentPane.add(passwordFieldPass2);
 
 		JButton jButtonErregistratu = new JButton("New button");
-		jButtonErregistratu.setBounds(164, 215, 105, 27);
+		jButtonErregistratu.setBounds(175, 205, 114, 27);
 		contentPane.add(jButtonErregistratu);
 
 	}
